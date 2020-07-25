3105f78ec6b9d84569ad3bb3e631ff8b46e8200c
nely de faria <nelly.nf54@gmail.com>
2020-07-25 19:35:27 -0300

    Adiciona arquivo aula02.php

---
+++ Curso-Arrays/aula02.php
@@ -0,0 +1,16 @@
+<?php
+namespace Alura;
+
+$saldos = [2500, 3000, 4400, 1000, 8700, 9000];
+
+foreach ($saldos as $saldo) {
+    echo "<p> O saldo é $saldo</p>";
+}
+
+
+sort($saldos); // Ordena vetores
+
+var_dump($saldos);
+
+echo "O menor saldo é $saldos[0]";
+
